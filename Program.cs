6e97c681ef49769504b74a6b1767a57643411e98
Finlay Christ <RandomiaGaming@gmail.com>
2024-01-25 01:32:30 -0800

    Backup commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace GimmeAccess
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string dirPath = "D:\\Windows";
+            DirectorySecurity directorySecurity = Directory.GetAccessControl(dirPath)
+            directorySecurity.SetOwner(new NTAccount(Environment.UserDomainName, "RandomiaGaming"));
+            Directory.SetAccessControl(dirPath, directorySecurity);
+            Directory.
+        }
+    }
+}
